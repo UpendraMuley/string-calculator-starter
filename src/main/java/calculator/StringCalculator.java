43def9c6589d3afc0a231a77d4d59085e5be8648
UpendraMuley <upendramul.1996@gmail.com>
2021-10-16 23:30:09 +0530

    allow add method to handle an unknown amount of numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,13 +4,19 @@ class StringCalculator {
 
     public int add(String input) {
     	if(input.isEmpty()) {
-       	  return 0;
-          }else if(input.contains(",")) {
-       	   String[] str = input.split(",");
-       	   return Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
-          }
-          else {
-       	   return Integer.parseInt(input);
-          }
-     }
+    	       	  return 0;
+    	          }else if(input.contains(",")) {
+    	       	   String[] str = input.split(",");
+    	       	   return toInt(str[0]) + toInt(str[1]);
+    	         }
+    	          else {
+    	       	   return toInt(input);
+    	          }
+    	    	
+    	    	}
+    	    
+    		private int toInt(String input) throws NumberFormatException {
+    			return Integer.parseInt(input);
+    		}
+     
 }
