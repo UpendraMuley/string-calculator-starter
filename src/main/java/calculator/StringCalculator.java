33178d6729fe60d13d173c24dd305db37fe896e7
UpendraMuley <upendramul.1996@gmail.com>
2021-10-16 23:25:16 +0530

    string_with_two_number_should_return_sum_as_int

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-     if(input.isEmpty()) {
-    	   return 0;
-       } else {
-    	   return Integer.parseInt(input);
-      }
-	
+    	if(input.isEmpty()) {
+       	  return 0;
+          }else if(input.contains(",")) {
+       	   String[] str = input.split(",");
+       	   return Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
+          }
+          else {
+       	   return Integer.parseInt(input);
+          }
      }
 }
