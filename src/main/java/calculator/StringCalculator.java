ca6cbc3da472cebce90a0323b8b386bced990b9d
UpendraMuley <upendramul.1996@gmail.com>
2021-10-17 15:02:48 +0530

    handle_NewLines_Between_Numbers_Return_sum_as_int

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,32 +1,46 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.tools.javac.util.Convert;
+
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if(input.isEmpty()) {
-    	       	  return 0;
-    	          }else {
-    	       	   String[] str = input.split(",");
-    	       	   
-    	       	   List<Integer> list = convert(str,toInt());
-    	       	   return sum(list).intValue();
-    	         }
-    	    	
-    	    	}
-    public static Converter<String, Integer> toInt(){
-    	return new Convert<String, Integer>(){
-    		public Integer convert(String from) {
-    			return toInt(from);
-    		}
-    	};
-    }
-    	    
-    		private int toInt(String input) throws NumberFormatException {
-    			return Integer.parseInt(input);
-    		}
+  
+	public static int add(String input) throws Exception{
+		String[] inputs = input.split("[-.;,*%&#@^\n]+");
+		int exception =0;
+		List<Integer> negNumbers = new ArrayList<>();
+		
+		int res = 0;
+		if("".equals(input) || null ==input) {
+			return 0;
+		}
+		for(int i=0; i<inputs.length;i++) {
+			if(Integer.parseInt(inputs[i]) < 0) {
+				exception = 1;
+				negNumbers.add(Integer.parseInt(inputs[i]));
+			}
+			if(exception !=1) {
+				res = res + ((Integer.parseInt(inputs[i]) >1000)?0:Integer.parseInt(inputs[i]));
+			}
+		}
+		if(exception==1) {
+			throw new Exception("Negatives not allowed "+negNumbers);
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringCalculator stringCalculator = new StringCalculator();
+		try {
+			System.out.println("Output: "+StringCalculator.add("2**1,\n2"));
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
      
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -9,23 +9,50 @@ class StringCalculatorShould {
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        try {
+			assertEquals(0, stringCalculator.add(""));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        try {
+			assertEquals(1, stringCalculator.add("1"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     @Test
     void string_with_two_number_should_return_sum_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3, stringCalculator.add("1,2"));
+        try {
+			assertEquals(3, stringCalculator.add("1,2"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     @Test
     void string_with_multple_number_should_return_sum_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(6, stringCalculator.add("1,2,3"));
+        try {
+			assertEquals(6, stringCalculator.add("1,2,3"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+
+    @Test
+    public void handle_NewLines_Between_Numbers_Return_sum_as_int()throws Exception {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(9,stringCalculator.add("2\n4,3"));
     }
 }
 
